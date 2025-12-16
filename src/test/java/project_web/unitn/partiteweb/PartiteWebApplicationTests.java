34ea4b88a3b9e95b343735e6a87f0af7c5255add
Francesco Annunziata <annunziata.francesco@outlook.it>
2025-12-16 17:01:21 +0100

    Initial commit

---
+++ src/test/java/project_web/unitn/partiteweb/PartiteWebApplicationTests.java
@@ -0,0 +1,13 @@
+package project_web.unitn.partiteweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PartiteWebApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
