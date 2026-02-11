8c4b339c2e3968250c0350000feb91b08a79ac5c
gabriele-delcoco <gabriele.delcoco@studenti.unitn.it>
2026-02-11 15:06:40 +0100

    Pom e starter per il deploy

--- src/main/java/project_web/unitn/partiteweb/PartiteWebApplication.java
+++ src/main/java/project_web/unitn/partiteweb/PartiteWebApplication.java
@@ -2,12 +2,18 @@ package project_web.unitn.partiteweb;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class PartiteWebApplication {
+public class PartiteWebApplication extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
         SpringApplication.run(PartiteWebApplication.class, args);
     }
 
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(PartiteWebApplication.class);
+    }
 }
